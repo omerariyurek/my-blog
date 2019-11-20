ae861527cb4648e5863ec4e50c7e61ac54a8d7bc
omerariyurek <aariyurekomer@gmail.com>
2019-11-20 04:34:25 +0300

    Added new methods

--- MyBlog.Business/Abstract/IPostService.cs
+++ MyBlog.Business/Abstract/IPostService.cs
@@ -1,10 +1,18 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using MyBlog.Core.Utilities.Results.Abstract;
+using MyBlog.Entities.Concrete;
 
 namespace MyBlog.Business.Abstract
 {
 	public interface IPostService
 	{
+		IDataResult<Post> GetById(int postId);
+		IDataResult<List<Post>> GetAll();
+		IDataResult<List<Post>> GetListByCategory(int categoryId);
+		IResult Add(Post post);
+		IResult Delete(Post post);
+		IResult Update(Post post);
 	}
 }
--- MyBlog.Business/Concrete/Managers/PostManager.cs
+++ MyBlog.Business/Concrete/Managers/PostManager.cs
@@ -2,10 +2,58 @@
 using System.Collections.Generic;
 using System.Text;
 using MyBlog.Business.Abstract;
+using MyBlog.Core.Utilities.Results.Abstract;
+using MyBlog.Core.Utilities.Results.Concrete;
+using MyBlog.DataAccess.Abstract;
+using MyBlog.Entities.Concrete;
 
 namespace MyBlog.Business.Concrete.Managers
 {
 	public class PostManager : IPostService
 	{
+		private IPostDal _postDal;
+		public PostManager(IPostDal postDal)
+		{
+			_postDal = postDal;
+		}
+
+		public IDataResult<Post> GetById(int postId)
+		{
+			try
+			{
+				var post = _postDal.Get(x => x.PostId == postId);
+				return new SuccessDataResult<Post>(post);
+			}
+			catch
+			{
+				return new ErrorDataResult<Post>("Post getirilemedi!");
+			}
+			
+		}
+
+		public IDataResult<List<Post>> GetAll()
+		{
+			throw new NotImplementedException();
+		}
+
+		public IDataResult<List<Post>> GetListByCategory(int categoryId)
+		{
+			throw new NotImplementedException();
+		}
+
+		public IResult Add(Post post)
+		{
+			throw new NotImplementedException();
+		}
+
+		public IResult Delete(Post post)
+		{
+			throw new NotImplementedException();
+		}
+
+		public IResult Update(Post post)
+		{
+			throw new NotImplementedException();
+		}
 	}
 }
