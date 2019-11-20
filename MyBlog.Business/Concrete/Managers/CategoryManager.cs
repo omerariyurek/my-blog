71436997799e0092243861d294e326282852c2f6
omerariyurek <aariyurekomer@gmail.com>
2019-11-20 04:20:09 +0300

    Success and error system added

---
+++ MyBlog.Business/Concrete/Managers/CategoryManager.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using MyBlog.Business.Abstract;
+
+namespace MyBlog.Business.Concrete.Managers
+{
+	public class CategoryManager : ICategoryService
+	{
+	}
+}
---
+++ MyBlog.Core/Utilities/Results/Concrete/Result.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using MyBlog.Core.Utilities.Results.Abstract;
+
+namespace MyBlog.Core.Utilities.Results.Concrete
+{
+	public class Result : IResult
+	{
+		public Result(bool success,string message):this(success)
+		{
+			Message = message;
+		}
+
+		public Result(bool success)
+		{
+			Success = success;
+		}
+		public bool Success { get; }
+		public string Message { get; }
+	}
+}
