bc2e937583351fb5da8962570095cb01c837726b
omerariyurek <aariyurekomer@gmail.com>
2019-11-21 04:51:02 +0300

    Messages class added

--- MyBlog.Business/Concrete/Managers/PostManager.cs
+++ MyBlog.Business/Concrete/Managers/PostManager.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using MyBlog.Business.Abstract;
+using MyBlog.Business.Constants;
 using MyBlog.Business.ValidationRules.FluentValidation;
 using MyBlog.Core.Aspects.Autofac.Caching;
 using MyBlog.Core.Aspects.Autofac.Validation;
@@ -38,19 +39,23 @@ namespace MyBlog.Business.Concrete.Managers
 		public IResult Add(Post post)
 		{
 			_postDal.Add(post);
-			return new SuccessResult("Post başarıyla kaydedildi.");
+			return new SuccessResult(Messages.PostAdded);
 		}
 
+		[CacheRemoveAspect("IPostService.Get")]
 		public IResult Delete(Post post)
 		{
 			_postDal.Delete(post);
-			return new SuccessResult("Post başarıyla silindi.");
+			return new SuccessResult(Messages.PostDeleted);
 		}
 
+
+		[ValidationAspect(typeof(PostValidator), Priority = 1)]
+		[CacheRemoveAspect("IPostService.Get")]
 		public IResult Update(Post post)
 		{
 			_postDal.Update(post);
-			return new SuccessResult("Post başarıyla güncellendi.");
+			return new SuccessResult(Messages.PostUpdated);
 		}
 	}
 }
---
+++ MyBlog.Business/Constants/Messages.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyBlog.Business.Constants
+{
+	public static class Messages
+	{
+		public static string PostAdded = "Post successfully added!";
+		public static string PostUpdated = "Post successfully updated!";
+		public static string PostDeleted = "Post successfully deleted!";
+	}
+}
